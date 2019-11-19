38963195d42099be1b8b757587e0b40d5238cb36
Emilio Araos <ea3dev@gmail.com>
2019-11-19 14:03:24 -0700

    setting up repo

---
+++ src/com/emilioaraos/Main.java
@@ -0,0 +1,10 @@
+package com.emilioaraos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Series.fibonacci(100));
+        System.out.println(Series.factorial(5));
+    }
+}
